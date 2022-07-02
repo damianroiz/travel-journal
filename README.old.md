f6258c8bfe8b660e273aa35492077e484a598cf6
Damian Roiz <damianx989@gmail.com>
2022-07-01 21:40:39 -0400

    navbar and images

--- README.old.md
+++
@@ -1,7 +0,0 @@
-# travel-journal
-
-This is the final projec of the second module of Scrimba's React Course For Begginers.
-
-## Instructions
-
-Create a Traver Journal using React props to populate components (blog cards) from data stored in a separate file
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <nav>
+                <img src="./images/world.png" alt="logo"></img>
+                <p>My Travel Journal</p>
+            </nav>
+        </div>
+    )
+}
